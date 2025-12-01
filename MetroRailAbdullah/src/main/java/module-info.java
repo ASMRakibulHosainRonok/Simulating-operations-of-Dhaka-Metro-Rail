1bacbbba87987be8b74f36e204a3489c92a40588
abdul96-rgb <abdulnahiyan96@gmail.com>
2025-12-01 20:15:36 -0330

    third commit

---
+++ MetroRailAbdullah/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module csc305.metrorailabdullah {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens csc305.metrorailabdullah to javafx.fxml;
+    exports csc305.metrorailabdullah;
+}
