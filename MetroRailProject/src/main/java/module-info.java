cad983e06df70574778f190868746ea8bf6932f5
abdul96-rgb <abdulnahiyan96@gmail.com>
2025-12-01 19:17:48 -0330

    Delete MetroRailProject directory

--- MetroRailProject/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module csc305.metrorailproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens csc305.metrorailproject to javafx.fxml;
-    exports csc305.metrorailproject;
-}
