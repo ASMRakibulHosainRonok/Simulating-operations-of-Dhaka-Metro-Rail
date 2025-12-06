f91a3bd9f0b5976f2439de6f6dde20356a7a7b31
A.S.M Rakibul Hosain Ronok <90785116+ASMRakibulHosainRonok@users.noreply.github.com>
2025-12-06 15:27:45 +0600

    commit

--- metro-lostfound/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.metrolostfound {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-
-    opens com.example.metrolostfound to javafx.fxml;
-    exports com.example.metrolostfound;
-}
